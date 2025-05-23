a1cea56645d45e14b17181389e0e8a19e47e4afb
Juhan Oskar Hennoste <juhanoskar@gmail.com>
2025-05-23 03:53:56 +0300

    Add server communication example in Python

---
+++ server_example.py
@@ -0,0 +1,34 @@
+import json
+from websockets.sync.client import connect
+from websockets.sync.connection import Connection
+
+
+# Before running this script start the TASmaniac WebSocket server by running launch_tasmaniac_server.bat.
+# Required libraries: `pip install websockets`.
+
+def play_level(websocket: Connection, level: int, inputs: list[str]) -> tuple[bool, int]:
+    """
+    Play a level with the given inputs and return a result indicating
+    if the level was completed successfully and how many ticks it took to finish.
+    """
+
+    websocket.send(json.dumps({'command': 'play_level', 'level': level, 'inputs': inputs}))
+    response = json.loads(websocket.recv(decode=True))
+
+    if response['status'] != 'executed':
+        raise AssertionError(f"Unexpected response from TAS: {response}")
+    if response['duration_ticks'] <= 0:
+        raise AssertionError(f"Suspicious duration: {response['duration_ticks']}")
+
+    return response['level_completed'], response['duration_ticks']
+
+if __name__ == '__main__':
+
+    level = 22
+    inputs_file = 'recordings/lvl022_05.32.txt'
+
+    with open(inputs_file, mode='r') as f:
+        inputs = f.read().splitlines()
+
+    with connect('ws://localhost:7111') as websocket:
+        print(play_level(websocket, level, inputs))
