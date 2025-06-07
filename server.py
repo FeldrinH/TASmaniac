40d85267258a6cd15ea0cb8f7c378e15d989f343
Juhan Oskar Hennoste <juhanoskar@gmail.com>
2025-06-08 01:10:49 +0300

    Add Python helper class to run multiple TASmaniac servers in parallel

--- server.py
+++ server.py
@@ -1,10 +1,62 @@
 import json
+import subprocess
+from threading import Lock
+from concurrent.futures import Future, ThreadPoolExecutor
+from contextvars import ContextVar
+from typing import Callable, Iterable
 import websockets.sync.client as client
 from websockets.sync.connection import Connection
 
 
 # Before running this script start the TASmaniac WebSocket server by running launch_tasmaniac_server.bat.
 # Required libraries: `pip install websockets`.
+
+
+_lock = Lock()
+_next_port = 7112
+
+_connection = ContextVar[Connection]('connection')
+
+class TASExecutor:
+    """
+    Thread pool executor that starts a new TASmaniac server for each worker thread.
+    The corresponding connection is passed as an argument to functions executed using `submit` and `map`.
+    This allows you to run multiple commands in parallel on separate servers, to take better advantage of having multiple CPU cores.
+    """
+
+    def __init__(self, max_workers: int) -> None:
+        self._max_workers = max_workers
+        self._executor = ThreadPoolExecutor(max_workers=max_workers, initializer=self._create_connection)
+        self._processes = []
+        self._connections = []
+    
+    def _create_connection(self):
+        global _next_port
+        with _lock:
+            port = _next_port
+            _next_port += 1
+            process = subprocess.Popen(['.\\launch_tasmaniac_server.bat', f'--server={port}'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            self._processes.append(process)
+            connection = connect(f'ws://localhost:{port}')
+            self._connections.append(connection)
+        _connection.set(connection)
+    
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._executor.shutdown(wait=True, cancel_futures=True)
+        for connection in self._connections:
+            connection.close()
+        for process in self._processes:
+            process.terminate() # TODO: This does not work on Windows due to some quirk with how .bat files start subprocesses
+    
+    def submit[T](self, fn: Callable[[Connection], T]) -> Future[T]:
+        return self._executor.submit(lambda: fn(_connection.get()))
+
+    def map[V, T](self, fn: Callable[[Connection, V], T], iterable: Iterable[V]) -> Iterable[T]:
+        return self._executor.map(lambda v: fn(_connection.get(), v), iterable)
+
 
 def connect(url = 'ws://localhost:7111') -> Connection:
     """
